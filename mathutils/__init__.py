f0c0e8140e9b498d50ea1ab086cdfc408deca0a0
MaxanceV <maxance.villame@gmail.com>
2025-11-12 18:12:31 +0100

    style: wrap imports and __all__ to satisfy flake8 E501

--- mathutils/__init__.py
+++ mathutils/__init__.py
@@ -1,7 +1,23 @@
 """Simple mathematical utilities for CI/CD learning."""
 
-from .calculator import add, divide, factorial, multiply, power, square_root, subtract
+from .calculator import (
+    add,
+    divide,
+    factorial,
+    multiply,
+    power,
+    square_root,
+    subtract,
+)
 
-__all__ = ["add", "subtract", "multiply", "divide", "power", "square_root", "factorial"]
+__all__ = [
+    "add",
+    "subtract",
+    "multiply",
+    "divide",
+    "power",
+    "square_root",
+    "factorial",
+]
 
 __version__ = "0.1.0"
